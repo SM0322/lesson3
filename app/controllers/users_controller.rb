082e080589747d9df5a350edf9efb9a12a575945
EC2 Default User <ec2-user@ip-172-31-37-254.ap-northeast-1.compute.internal>
2022-12-15 06:42:01 +0000

    kinou kansei

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
       redirect_to user_path(@user.id)
       flash[:notice] = "You have updated user successfully."
     else
-      render :show
+      render :edit
     end
   end
 
@@ -30,6 +30,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :profile_image, presence: true
+  validates :introduction, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
