188c9c63b04f579472ed1189c0357003b5a91a95
EC2 Default User <ec2-user@ip-172-31-9-127.ap-northeast-1.compute.internal>
2023-01-05 00:36:06 +0000

    Oyo4

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,12 @@ class User < ApplicationRecord
   has_many :favorit1es, dependent: :destroy
 
   has_one_attached :profile_image
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   def get_profile_image(width, height)
     unless profile_image.attached?
@@ -21,4 +27,17 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+  
+def follow(user_id)
+  relationships.create(followed_id: user_id)
 end
+
+def unfollow(user_id)
+  relationships.find_by(followed_id: user_id).destroy
+end
+
+def following?(user)
+  followings.include?(user)
+end
+  
+end 
