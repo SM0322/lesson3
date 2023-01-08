a28513525c6472c802c2e2bf9b91b6ce5e393192
EC2 Default User <ec2-user@ip-172-31-9-127.ap-northeast-1.compute.internal>
2023-01-08 04:39:40 +0000

    Oyo7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ before_action :ensure_current_user, {only: [:edit, :update]}
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page])
     @book = Book.new
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def edit
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,14 @@ import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
